0f27c2d6a771ee17335bce1680c437f76cdddd06
choran <cathal.horan@intercom.io>
2016-05-13 10:53:05 +0000

    Updated with event webhook setup

--- webhook_tasks.rb
+++ webhook_tasks.rb
@@ -5,11 +5,36 @@ class WebhookTasks < IntercomClient
     #Leave this empty as simple over-ride
   end
 
-  def create_webhook(url, topics)
-    #Create a hash to pass through the Webhook data
-    webhook = {:url => url,
-               :topics => topics}
-    @@intercom.subscriptions.create(webhook)
+  def create_webhook(url, topics, events=false)
+    if events
+      event_data = {
+          service_type: "web",
+          topics: ["event.created"],
+          url: url,
+          metadata: {
+          event_names: events
+          }
+      }
+      #Need to POST event metadata for event.created topic
+      @@intercom.post("/subscriptions/", event_data)
+    else
+      #Create a hash to pass through the Webhook data
+      webhook = {:url => url,
+                 :topics => topics}
+      @@intercom.subscriptions.create(webhook)
+    end
+  end
+
+  def update_webhook(id, url, new_topics, events=false)
+    #Create the data you are going to POST
+    updates = {
+        topics: new_topics,
+        url: url,
+        }
+    if events
+      updates[:metadata] = {event_names: events}
+    end
+    @@intercom.post("/subscriptions/#{id}", updates)
   end
 
   def list_subscriptions(attrib=false)

0ecebf8fc25a3786044287b4d4ca7c33504c3187
choran <cathal.horan@intercom.io>
2016-05-18 11:30:23 +0000

    Adding tagging and error handling

--- sample_data.rb
+++ sample_data.rb
@@ -7,6 +7,7 @@ def sample_data(csv_file, app_id, api_key)
   IntercomClient.new(app_id, api_key)
   #Instantiate your user class
   usr = UserTasks.new
+  tag = TagTasks.new
 
   #List of user attribtues
   user_attributes = ["email","name","user_id","id","signed_up_at",
@@ -14,24 +15,33 @@ def sample_data(csv_file, app_id, api_key)
                      "companies","last_request_at"]
   #Iterate through each row and check for user attributes
   CSV.foreach(csv_file, headers:true) do |row|
-    user_data = {}
-    for attrib in user_attributes
-      if not row[attrib].nil?
-        user_data[attrib.to_sym] = row[attrib]
+    begin
+      user_data = {}
+      for attrib in user_attributes
+        if not row[attrib].nil?
+          user_data[attrib.to_sym] = row[attrib]
+        end
       end
-    end
-    if not row['custom_attributes'].nil?
-      #puts row.inspect
-      custom_data = {}
-      for line in row['custom_attributes'].split(';')
-        vals = line.split(':')
-        custom_data[vals[0].to_sym] = vals[1]
+      if not row['custom_attributes'].nil?
+        #puts row.inspect
+        custom_data = {}
+        for line in row['custom_attributes'].split(';')
+          vals = line.split(':')
+          custom_data[vals[0].to_sym] = vals[1]
+        end
+        user_data['custom_attributes'] = custom_data
       end
-      user_data['custom_attributes'] = custom_data
-    end
-    #Create a user with these attributes
-    usr.create_user(user_data)
-    #puts user_data
+      #Create a user with these attributes
+      usr.create_user(user_data)
+      #puts user_data
+      puts "Creating user:#{row['email']}"
+      #Create tags for each grouping of Users based on the
+      # the email address of the users
+      tag_names = row['email'].split(/['@''.']/)
+      tag.tag_user(row['user_id'], tag_names[1])
+      rescue NoMethodError, Intercom::BadRequestError => e
+        puts "ERROR Creating Users #{e.message}"
+      end
   end
 end
 

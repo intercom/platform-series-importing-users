5454e1e63bb21a8395bf46aff2ddf95782c47ecd
choran <cathal.horan@intercom.io>
2016-05-31 11:24:23 +0000

    Adding update and convert tasks

--- lead_tasks.rb
+++ lead_tasks.rb
@@ -1,4 +1,5 @@
 require './intercom_client'
+require './user_tasks'
 
 class LeadTasks < IntercomClient
   def initialize
@@ -38,10 +39,54 @@ class LeadTasks < IntercomClient
   end
 
   def update_lead_attrib(id_or_user_id, attrib=false, value=false)
-    #Create new custom attributes for a lead or update existing ones
+    #First searchf or the lead and then check if it is custom or standard attribute
     lead = find_leads(id_or_user_id)
-    lead.custom_attributes[attrib] = value
+    begin
+      #This will throw exception if not standard attribute
+      lead.send(attrib)
+      #If it is standard the simply set it to value
+      lead.send("#{attrib}=", value)
+    rescue Intercom::AttributeNotSetError
+      #If not standard attribute then assume it is a custom attribute
+      lead.custom_attributes[attrib] = value
+    end
+    #Either way we need to save the changed attribute at the end
     @@intercom.contacts.save(lead)
   end
 
+  def convert_leads(lead_id_or_id, lead_email, options={})
+    #If no lead is found an exception will be returned
+    lead = find_leads(lead_id_or_id)
+    #Set some defaults if not set by user
+    defaults ={
+        :user_id => nil,
+        :email => lead_email,
+        :name => nil,
+    }
+    options = defaults.merge(options)
+    #Create new Json object to pass through to endpoint
+    new_user = {
+        contact: {
+            :user_id => lead.user_id,
+        },
+        user: {}
+    }
+    new_user = new_user.merge(:user => options)
+
+    @@intercom.post("/contacts/convert", new_user)
+  end
+
+  def merge_leads(lead_id_or_id, user_criteria)
+    lead = find_leads(lead_id_or_id)
+    #Check if the user exists
+    usr = UserTasks.new
+    user = usr.find_user(user_criteria)
+
+    @@intercom.contacts.convert(lead, user)
+  end
+
+  def delete_lead(contact_id_or_id)
+    lead = find_leads(contact_id_or_id)
+    @@intercom.contacts.delete(lead)
+  end
 end

674f24d2a806480bb05b6fa664344d356375ba60
Timothy Lim <tim.lim@intercom.io>
2018-02-01 08:59:12 +0800

    Update to use tokens instead of API keys

--- intercom_client.rb
+++ intercom_client.rb
@@ -2,8 +2,8 @@ require 'intercom'
 
 class IntercomClient
   #First lets setup the client with your App ID and App Key
-  def initialize(your_app_id, your_api_key)
-    @@intercom = Intercom::Client.new(app_id: your_app_id, api_key: your_api_key)
+  def initialize(token)
+    @@intercom = Intercom::Client.new(token: token)
   end
 end
 
